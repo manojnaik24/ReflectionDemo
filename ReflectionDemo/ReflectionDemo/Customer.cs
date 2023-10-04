122341bff9fc3bbd2fe96bfbf432e0d3c2cb9b59
Manoj naik <chittinaik@gamil.com>
2023-10-04 20:16:41 +0530

    [Manoj] add:Reflection

---
+++ ReflectionDemo/ReflectionDemo/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class Customer
+    {
+        public int a,b;
+        public int Id { get; set; }
+
+        public string Name { get; set; }    
+
+
+        Customer() {
+
+            Id = 1;
+            Name = "Manoj kaumr naik";
+
+        }
+        public  Customer(int ID,string Name)
+        {
+            this.Id = ID;
+            this.Name = Name;
+
+        }
+        public void CustomerFirstMethod()
+        {
+
+        }
+        public void CustomerSecondMethod(int Id,string name)
+        {
+
+        }
+        
+    }
+}
---
+++ ReflectionDemo/ReflectionDemo/RefectionInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    internal class RefectionInfo
+    {
+        public void RefecMethod()
+        {
+            Type type = Type.GetType("ReflectionDemo.Customer");
+            Console.WriteLine(type.FullName);
+            Console.WriteLine(type.Name);
+            Console.WriteLine(type.Namespace);
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name+""+property.PropertyType.Name);
+
+            }
+            ConstructorInfo[] constructorInfos = type.GetConstructors();
+            foreach (ConstructorInfo constructorInfo in constructorInfos)
+            {
+                Console.WriteLine(constructorInfo.ToString());
+            }
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name+" "+method.ReturnType.Name);
+            }
+        }
+    }
+}
